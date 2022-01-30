c4343970dd8d56979c2ca462442023d96fe4f059
AmanSingh137 <65694984+AmanSingh137@users.noreply.github.com>
2022-01-30 13:57:25 +0530

    google sheets integrated for addressing complaints

--- src/Complaints.js
+++ src/Complaints.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card, InputGroup, FormControl } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Complaints = () => {
-    
+    const [complaint, setComplaint] = useState('');
+    const handleSubmit = (e) => {
+        const Complains = {
+            Complaints: complaint
+        }
+        axios.post('https://sheet.best/api/sheets/6ff549e8-66b5-4da0-a7f9-08216534e164', Complains).then(()=>{
+            setComplaint('');
+        })
+    }
     return (
         <Card style={{width: '50rem'}} className="mx-auto mt-5">
             <InputGroup className="mb-3" >
@@ -11,9 +20,10 @@ const Complaints = () => {
                     placeholder="Enter your complaint"
                     aria-label="Username"
                     aria-describedby="basic-addon1"
+                    onChange={(e)=>setComplaint(e.target.value)}
                 />
             </InputGroup>
-            <Link to="/success" className="btn btn-primary" >Submit</Link>
+            <Link to="/success" className="btn btn-primary" onClick={handleSubmit} >Submit</Link>
         </Card>
     )
 }
